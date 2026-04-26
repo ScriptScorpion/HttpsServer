f8966b3a0db6c9997799b254d1421d06a8579a87
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2026-04-26 13:37:37 +0300

    Update certificate_gen.sh

--- src/certificate_gen.sh
+++ src/certificate_gen.sh
@@ -4,7 +4,11 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 # AUTOMATIC VERSION
 # CERT_KEY="strongkey"
-# openssl req -x509 -passout pass:$CERT_KEY -newkey rsa:4096 -keyout "$SCRIPT_DIR/key.pem" -out "$SCRIPT_DIR/cert.pem" -sha256 -days 365 -batch
+# openssl req -x509 -passout pass:$CERT_KEY -newkey rsa:4096 -keyout "$SCRIPT_DIR/key_enc.pem" -out "$SCRIPT_DIR/cert.pem" -sha256 -days 365 -batch
+# openssl rsa -passin pass:$CERT_KEY -in "$SCRIPT_DIR/key_enc.pem" -out "$SCRIPT_DIR/key.pem"
+# rm -f "$SCRIPT_DIR/key_enc.pem"
 
 # MANUAL VERSION
-openssl req -x509 -newkey rsa:4096 -keyout "$SCRIPT_DIR/key.pem" -out "$SCRIPT_DIR/cert.pem" -sha256 -days 365 -batch
+openssl req -x509 -newkey rsa:4096 -keyout "$SCRIPT_DIR/key_enc.pem" -out "$SCRIPT_DIR/cert.pem" -sha256 -days 365 -batch
+openssl rsa -in "$SCRIPT_DIR/key_enc.pem" -out "$SCRIPT_DIR/key.pem"
+rm -f "$SCRIPT_DIR/key_enc.pem"
